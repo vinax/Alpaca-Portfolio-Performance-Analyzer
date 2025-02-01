9c8512bb6daf69678045fb5ded959ca1295c6846
vinax <78116233+vinax@users.noreply.github.com>
2025-02-01 05:52:15 +0000

    Update Streamlit_Alpaca_Public_Dashboard.py

--- Streamlit_Alpaca_Public_Dashboard.py
+++ Streamlit_Alpaca_Public_Dashboard.py
@@ -33,7 +33,7 @@ import zipfile
 
 ################## MAIN ##################
 
-st.title("Alpaca Portfolio Dashboard Performance Analyzer") # Streamlit App Title
+st.title("Alpaca Portfolio Dashboard\nPerformance Analyzer") # Streamlit App Title
 st.write("### About")
 st.write("This is a basic web app/software tool designed to provide Alpaca users with a user-friendly way to obtain detailed analytic insights into their account portfolio history.")
 
